4a61779796e41e5538891e29bbe17c0f2cd0b8f0
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-10 00:23:42 +0300

    initial commit

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Routes } from 'react-router-dom'
+import home from './pages/home'
 
 const App = () => {
   return (
-    <div>
+    <div className='mx-4 sm:mx-[10%]'>
+
+      <Routes>
+        <Routes path='/' element={<home />} />
+      </Routes>
       
     </div>
   )
