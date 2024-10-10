133641592e1bc060d5e5fdb00d6d0932a227ea66
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-10 11:23:07 +0300

    initial commit

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 import Logo from '../assets/logo.svg'
 import { NavLink } from 'react-router-dom'
 
@@ -6,7 +7,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
         <img className='w-44 cursor-pointer' src={Logo} alt="Logo" />
-        <ul className='hidden md:flex items-start gap-5 font-medium'>
+        <ul className='flex-container'>
             <NavLink>
                 <li>HOME</li>
                 <hr />
