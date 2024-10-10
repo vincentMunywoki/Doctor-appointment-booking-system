593210985cbdd332deeec4d7f8fb38685dffd964
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-10 16:23:20 +0300

    initial commit

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -7,15 +7,21 @@ const Navbar = () => {
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
         <img className='w-44 cursor-pointer' src={Logo} alt="Logo" />
         <ul className='hidden md:flex items-start gap-5 font-medium'>
-            <NavLink>
+            <NavLink to='/'>
                 <li className='py-1'>HOME</li>
-                <hr className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto' />
+                <hr className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto hidden' />
+            </NavLink>
+            <NavLink to='/'>
                 <li className='py-1'>ALL DOCTORS</li>
-                <hr className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto' />
+                <hr className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto hidden' />
+            </NavLink>
+            <NavLink to='/'>
                 <li className='py-1'>ABOUT</li>
-                <hr className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto' />
+                <hr className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto hidden' />
+            </NavLink>
+            <NavLink to='/'>
                 <li className='py-1'>CONTACT</li>
-                <hr  className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto'/>
+                <hr  className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto hidden'/>
             </NavLink>
         </ul>
         <div>
