f66af45c5c2e42c9d2467c8c0bcb8f455b298583
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-10 11:57:02 +0300

    initial commit

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css'
 import Logo from '../assets/logo.svg'
 import { NavLink } from 'react-router-dom'
 
@@ -7,16 +6,16 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
         <img className='w-44 cursor-pointer' src={Logo} alt="Logo" />
-        <ul className='flex-container'>
+        <ul className='hidden md:flex items-start gap-5 font-medium'>
             <NavLink>
-                <li>HOME</li>
-                <hr />
-                <li>ALL DOCTORS</li>
-                <hr />
-                <li>ABOUT</li>
-                <hr />
-                <li>CONTACT</li>
-                <hr />
+                <li className='py-1'>HOME</li>
+                <hr className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto' />
+                <li className='py-1'>ALL DOCTORS</li>
+                <hr className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto' />
+                <li className='py-1'>ABOUT</li>
+                <hr className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto' />
+                <li className='py-1'>CONTACT</li>
+                <hr  className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto'/>
             </NavLink>
         </ul>
         <div>
