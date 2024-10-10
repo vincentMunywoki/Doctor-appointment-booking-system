be528a0e49055457b5f1a2666ba649ce116e8cdb
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-10 17:43:01 +0300

    initial commit

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -34,9 +34,17 @@ const Navbar = () => {
         <div className='flex items-center gap-4'>
             {
                 token 
-                ? <div>
-                    <img src={assets.profile_pic} alt="" />
-                    <img src={assets.dropdown_icon} alt="" />
+                ? <div className='flex items-center gap-2 cursor-pointer group relative' >
+                    <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
+                    <img className='w-2.5' src={assets.dropdown_icon} alt="" />
+                    <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block' >
+                        <div>
+                            <p>My Profile</p>
+                            <p>My Appointments</p>
+                            <p>Logout</p>
+                            
+                        </div>
+                    </div>
                 </div> 
                 :<button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
             }
