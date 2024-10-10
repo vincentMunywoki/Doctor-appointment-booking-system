194e8bc24076c21f80b3a46754fb3988cd715b2b
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-10 17:32:10 +0300

    initial commit

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,31 +1,46 @@
-import React from 'react'
-import Logo from '../assets/logo.svg'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react'
+import {assets} from '../assets/assets'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const [showMenu, setShowMenu] = useState(false)
+    const [token,setToken] = useState(true)
+
+
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-        <img className='w-44 cursor-pointer' src={Logo} alt="Logo" />
+        <img className='w-44 cursor-pointer' src={assets.logo} alt="Logo" />
         <ul className='hidden md:flex items-start gap-5 font-medium'>
             <NavLink to='/'>
                 <li className='py-1'>HOME</li>
                 <hr className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink to='/'>
+            <NavLink to='/doctors'>
                 <li className='py-1'>ALL DOCTORS</li>
                 <hr className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink to='/'>
+            <NavLink to='/about'>
                 <li className='py-1'>ABOUT</li>
                 <hr className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink to='/'>
+            <NavLink to='/contact'>
                 <li className='py-1'>CONTACT</li>
                 <hr  className='border-none outline-none h-0.5 bg-primary   w-3/5 m-auto hidden'/>
             </NavLink>
         </ul>
-        <div>
-            <button>Create Account</button>
+        <div className='flex items-center gap-4'>
+            {
+                token 
+                ? <div>
+                    <img src={assets.profile_pic} alt="" />
+                    <img src={assets.dropdown_icon} alt="" />
+                </div> 
+                :<button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
+            }
+            
         </div>
     </div>
   )
