75b6c5162830050a5937255e3b2e3e4f5c1cc046
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-12 00:08:55 +0300

    initial commit

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       colors:{
         'primary':"#5f6fff"
+      },
+      gridTemplateColumns:{
+        'auto':'repeat(auto-fill, minmax(200px, 1fr))'
       }
     },
   },
