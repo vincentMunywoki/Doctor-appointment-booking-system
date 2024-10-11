be906dda1206cb4f5a86598fc5c2f0b4316f156b
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-11 20:25:27 +0300

    initial commit

--- front-end/src/components/SpecialityMenu.jsx
+++ front-end/src/components/SpecialityMenu.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 
 const SpecialityMenu = () => {
   return (
-    <div id='speciality' >
-        <h1>Find my Speciality</h1>
-        <p>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
-        <div>
+    <div className='flex flex-col items-center gap-4 py-16 text-gray-800' id='speciality' >
+        <h1 className='text-3xl font-medium' >Find my Speciality</h1>
+        <p className='sm:w-1/3 text-center text-sm' >Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
+        <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll' >
             {specialityData.map((item,index)=>(
                 <Link key={index} to={'/doctors/${item.speciality}'} > 
                     <img src={item.image} alt=''/>   
