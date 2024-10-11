1c659832ce4ddf4fb2efc92e78780ccbfb10f912
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-12 00:23:31 +0300

    initial commit

--- front-end/src/components/TopDoctors.jsx
+++ front-end/src/components/TopDoctors.jsx
@@ -7,7 +7,7 @@ const TopDoctors = () => {
     const navigate = useNavigate()
 
   return (
-    <div className='flex flex-col item-center gap-4 my-16 text-gray-900 md:mx-10' >
+    <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10' >
         <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
         <p className='sm:w-1/3 text-center text sm'>Simply browse through our extensive list of trusted doctors.</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0' >
