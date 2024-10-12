c18df2ee82063000709acb8dff505bb3a6cbdac3
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-12 23:29:12 +0300

    initial commit

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -3,19 +3,19 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-        <div>
+    <div className='md:mx-10' >
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm' >
 
             {/*-----Left section-------*/}
             <div>
-                <img src={assets.logo} alt="" />
-                <p>QuickDoc is a revolutionary healthcare platform designed to simplify access to quality medical care. With QuickDoc, you can easily find top-rated doctors, book appointments, and receive personalized medical advice.</p>
+                <img className='mb-5 w-40 rounded-lg cursor-pointer' src={assets.logo} alt="" />
+                <p className='w-full md:w-2/3 text-gray-600 leading-6' > QuickDoc is a revolutionary healthcare platform designed to simplify access to quality medical care. With QuickDoc, you can easily find top-rated doctors, book appointments, and receive personalized medical advice.</p>
             </div>
 
             {/*-----center section-------*/}
             <div>
-                <p>COMPANY</p>   
-                <ul>
+                <p className='text-xl font-medium mb-5' >COMPANY</p>   
+                <ul className='flex flex-col gap-2 text-gray-600' >
                     <li>Home</li>
                     <li>About</li>
                     <li>Contact Us</li>
@@ -25,8 +25,8 @@ const Footer = () => {
 
             {/*-----Right section-------*/}
             <div>
-                <p>GET IN TOUCH</p>   
-                <ul>
+                <p className='text-xl font-medium mb-5' >GET IN TOUCH</p>   
+                <ul className='flex flex-col gap-2 text-gray-600' >
                     <li>+254702463477</li>
                     <li>vincentmunywoki12@gmail.com</li>
                 </ul>             
@@ -36,7 +36,7 @@ const Footer = () => {
         {/*--------Copyright Text---------*/}
         <div>
             <hr />
-            <p>Copyright 2024@ QuickDoc - All Rights Reserved</p>
+            <p className='py-5 text-sm text-center' >Copyright 2024@ QuickDoc - All Rights Reserved</p>
         </div>
 
     </div>
