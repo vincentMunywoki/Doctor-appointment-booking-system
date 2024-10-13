4338fda24d4ea8799b7cbf6a37c71f79dc82fa75
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-13 23:07:10 +0300

    initial commit

--- front-end/src/pages/Appointment.jsx
+++ front-end/src/pages/Appointment.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
 
 const Appointment = () => {
 
@@ -20,13 +21,36 @@ const Appointment = () => {
       fetchDocInfo() // call the function doc info
   },[doctors,docId])
 
-  
+
   return docInfo && (  // check if doct infor has a label using && operator
     <div>
       {/*----------Doctor details------------*/}
-      <div>
+      <div className='flex flex-col sm:flex-row gap-4'>
         <div>
-          <img src={docInfo.image} alt="" />
+          <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
+        </div>
+
+        <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px]sm:mt-0' >
+          {/*-------Doc info : Name, degree ,experience------- */}
+          <p className=' flex items-center gap-2 text-2xl font-medium text-gray-900' >
+            {docInfo.name} 
+            <img className='w-5' src={assets.verified_icon} alt="" />
+          </p>
+          <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
+            <p>{docInfo.degree} - {docInfo.speciality}</p>
+            <button className='py-0.5 px-2 border text-xs rounded-full' >{docInfo.experience}</button>
+          </div>
+
+          {/* -----Doctor About --------*/}
+          <div>
+            <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3' >
+              About <img src={assets.info_icon} alt="" />
+            </p>
+            <p className='text-sm text-gray-500 max-w-[700px] mt-1' >{docInfo.about}</p>
+          </div>
+          <p>
+            Appointment fee: <span>{docInfo.fees}</span>
+          </p>
         </div>
       </div>
         
