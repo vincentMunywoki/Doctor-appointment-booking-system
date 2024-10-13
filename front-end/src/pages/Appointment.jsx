4e6f928cb948b06fd4f5ea1a83ff23c62a86e8ab
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-13 22:39:37 +0300

    initial commit

--- front-end/src/pages/Appointment.jsx
+++ front-end/src/pages/Appointment.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Appointment = () => {
-  return (
+
+  // get doctors by docid
+  const {docId} = useParams()
+  const {doctors} = useContext(AppContext)
+
+  const [docInfo,setDocInfo] = useState(null)
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc => doc._id === docId) //create variable called docInfo to store doctors information
+    setDocInfo(docInfo)
+    console.log(docInfo)
+  }
+
+  useEffect(()=> {
+      fetchDocInfo() // call the function doc info
+  },[doctors,docId])
+
+  
+  return docInfo && (  // check if doct infor has a label using && operator
     <div>
+      {/*----------Doctor details------------*/}
+      <div>
+        <div>
+          <img src={docInfo.image} alt="" />
+        </div>
+      </div>
         
     </div>
   )
