1e0a8dbb5ace028ac15dacf350824a078b7cce18
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-16 13:41:03 +0300

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,8 +39,8 @@ const Navbar = () => {
                     <img className='w-2.5' src={assets.dropdown_icon} alt="" />
                     <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block' >
                         <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4' >
-                            <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer' >My Profile</p>
-                            <p onClick={()=>navigate('my-appointment')} className='hover:text-black cursor-pointer' >My Appointments</p>
+                            <p onClick={()=>navigate('myprofile')} className='hover:text-black cursor-pointer' >My Profile</p>
+                            <p onClick={()=>navigate('myappointment')} className='hover:text-black cursor-pointer' >My Appointments</p>
                             <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer' >Logout</p>
                             
                         </div>
--- src/pages/myprofile.jsx
+++ src/pages/myprofile.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const myprofile = () => {
+
+  const [userData,setUserData] = useState({
+    name:"Vincent Munywoki",
+    image:assets.profile_pic,
+    email:'vincentmunywoki@gmail.com',
+    phone:'+254 702 463 477',
+    address: {
+      line1: "PO BOX 6 60100",
+      line2: "Nairobi, Kenya"
+    },
+    gender:"male",
+    dob:'2000-05-05'
+  })
+
+
+  const[isEdit,setIsEdit] = useState(false)
+
+
   return (
     <div>
-        
+        <img src={userData.image} alt="" />
+
+        {
+          isEdit 
+          ? <input type = "text" />
+          : <p>{userData.name}</p>
+        }
+
     </div>
   )
 }
