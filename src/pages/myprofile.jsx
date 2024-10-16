7fb7cdd23208b3718a03c2c0b5198978fa1e94cf
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-16 14:17:32 +0300

    initial commit

--- src/pages/myprofile.jsx
+++ src/pages/myprofile.jsx
@@ -12,7 +12,7 @@ const myprofile = () => {
       line1: "PO BOX 6 60100",
       line2: "Nairobi, Kenya"
     },
-    gender:"male",
+    gender:"Male",
     dob:'2000-05-05'
   })
 
@@ -22,13 +22,56 @@ const myprofile = () => {
 
   return (
     <div>
-        <img src={userData.image} alt="" />
+      <img src={userData.image} alt="" />
 
-        {
-          isEdit 
-          ? <input type = "text" />
-          : <p>{userData.name}</p>
-        }
+      {
+        isEdit 
+        ? <input type = "text" value={userData.name} onChange={e => setUserData(prev => ({...prev,name:e.target.value}))}/>
+        : <p>{userData.name}</p>
+      }
+
+      <hr />
+      <div>
+        <p>CONTACT INFORMATION</p>
+        <div>
+          <p>Email Id:</p>
+          <p>{userData.email}</p>
+          <p>Phone:</p>
+          {
+            isEdit 
+            ? <input type = "text" value={userData.phone} onChange={e => setUserData(prev => ({...prev,phone:e.target.value}))}/>
+            : <p>{userData.phone}</p>
+          }
+          <p>Address:</p>
+          {
+            isEdit
+            ? <p>
+                 <input onChange={(e) => setUserData(prev => ({...prev, address: {...prev.address,line1: e.target.value}}))} value={userData.address.line1} type="text" /> 
+                 <br />
+                 <input onChange={(e) => setUserData(prev => ({...prev, address: {...prev.address,line2: e.target.value}}))} value={userData.address.line2}type="text" />      
+              </p>
+            : <p>
+              {userData.address.line1}
+              <br />
+              {userData.address.line2}
+            </p>
+          }
+        </div>
+      </div>
+      <div>
+        <p>BASIC INFORMATION</p>
+        <div>
+          <p>Gender:</p>
+          {
+            isEdit 
+              ? <select onChange={(e) => setUserData(prev => ({...prev, gender: e.target.value}))} value={userData.gender}>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+              : <p>{userData.gender}</p>
+          }
+        </div>
+      </div>
 
     </div>
   )
