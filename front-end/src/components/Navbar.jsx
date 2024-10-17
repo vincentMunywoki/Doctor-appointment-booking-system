42afedb46986ed6b3cbf3e69643676dce38cf126
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-17 21:27:31 +0300

    initial commit

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -39,8 +39,8 @@ const Navbar = () => {
                     <img className='w-2.5' src={assets.dropdown_icon} alt="" />
                     <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block' >
                         <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4' >
-                            <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer' >My Profile</p>
-                            <p onClick={()=>navigate('my-appointment')} className='hover:text-black cursor-pointer' >My Appointments</p>
+                            <p onClick={()=>navigate('myprofile')} className='hover:text-black cursor-pointer' >My Profile</p>
+                            <p onClick={()=>navigate('myappointment')} className='hover:text-black cursor-pointer' >My Appointments</p>
                             <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer' >Logout</p>
                             
                         </div>
@@ -48,6 +48,22 @@ const Navbar = () => {
                 </div> 
                 :<button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
             }
+            <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />   
+
+            {/**-------Mobile Menu-------*/}    
+            <div className={` ${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+                <div className='flex items-center justify-between px-5 py-6'>
+                    <img className='w-36 rounded-lg' src={assets.logo} alt="" />
+                    <img className='w-7' onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
+                </div>
+                <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium' >
+                    <NavLink  onClick={()=>setShowMenu(false)} to='/' ><p className='px-4 py-2 rounded inline-block'>HOME</p></NavLink>
+                    <NavLink  onClick={()=>setShowMenu(false)} to='/doctors' ><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
+                    <NavLink  onClick={()=>setShowMenu(false)} to='/about' ><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
+                    <NavLink  onClick={()=>setShowMenu(false)} to='/contact' ><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+                </ul>
+            </div>
+                 
             
         </div>
     </div>
--- front-end/src/components/RelatedDoctors.jsx
+++ front-end/src/components/RelatedDoctors.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import { useNavigate } from 'react-router-dom'
 
-const RelatedDoctors = (speciality,docId) => {
+const RelatedDoctors = ({speciality,docId}) => {
 
     const {doctors} = useContext(AppContext)
     const navigate = useNavigate()
@@ -18,11 +18,11 @@ const RelatedDoctors = (speciality,docId) => {
 
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10' >
-        <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
+        <h1 className='text-3xl font-medium'>Related Doctors</h1>
         <p className='sm:w-1/3 text-center text sm'>Simply browse through our extensive list of trusted doctors.</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0' >
             {relDoc.slice(0,5).map((item,index)=>(
-                <div onClick={()=>navigate(`/appointment/${item._id}`)}className='border border-blue-200 rounded-x1 overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index} >
+                <div onClick={()=>{navigate(`/appointment/${item._id}`);scrollTo(0,0)}}className='border border-blue-200 rounded-x1 overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index} >
                     <img className='bg-blue-50' src={item.image} alt="" />
                     <div className='p-4' >
                         <div className='flex items-center gap-2 text-sm text-center text-green-500' >
