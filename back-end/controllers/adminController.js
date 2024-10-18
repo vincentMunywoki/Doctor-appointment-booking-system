1afbe39c6ddc1de9709804f04a72c54118510630
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-18 17:38:10 +0300

    initial commit

--- back-end/controllers/adminController.js
+++ back-end/controllers/adminController.js
@@ -1,4 +1,7 @@
-
+import validator from "validator"
+import bcrypt from 'bcrypt'
+import { v2 as cloudinary } from "cloudinary"
+import doctorModel from "../models/doctorModel.js"
 
 
 // API for ading doctor
@@ -7,9 +10,56 @@ const addDoctor = async (req,res) => {
     try {
 
         const{ name, email, password, speciality, degree, experience, about, fees, address} = req.body
+        const imageFile = req.imageFile
 
+        //checking for all data to add doctor
+        if (!name || !email || !password || !speciality || !degree || !experience || !about ||fees || !address) {
+            return resjon({success:false,message:"Missing details"})
+        }
+        
+        // validating email format
+        if (!validator.isEmail(email)) {
+            return resjon({success:false,message:"Please enter a valid email"})
+        }
+
+        // validating strong password
+        if(password.length < 8){
+            return resjon({success:false,message:"please enter a strong pasword"})
+        }
+
+        // password Encription Hashing doc password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+
+        // upload image to cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:"image"})
+        const imageUrl = imageUpload.secure_url
+
+        const doctorData = {
+            name,
+            email,
+            image:imageUrl,
+            password:hashedPassword,
+            speciality,
+            degree,
+            experience,
+            about,
+            fees,
+            address:JSON.parse(address),
+            date:Date.now()
+        }
+
+        const newDoctor = new doctorModel(doctorData)
+        await newDoctor.save()
+
+
+        res.json({success:true,message:"Doctor Added"})
+
+        
     } catch (error) {
-
+        console.log(error)
+        res.json({success:false,message:error.message})
     }
 
 }
--- back-end/server.js
+++ back-end/server.js
@@ -5,7 +5,6 @@ import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import adminRouter from './routes/adminRoute.js'
 
-
 // App config
 const app = express()
 const port = process.env.PORT || 4000
@@ -18,6 +17,7 @@ app.use(cors())
 
 // api endpoints
 app.use('/api/admin',adminRouter)
+// localhost:4000/api/admin/add-doctor
 
 app.get('/',(req,res)=>{
     res.send('API WORKING')
