f3a3854fc18d0bf4f50f5765ecde6e62e619e132
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-18 13:20:02 +0300

    initial commit

---
+++ back-end/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+
+
+// App config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+app.get('/',(req,res)=>{
+    res.send('API WORKING')
+})
+
+app.listen(port,()=> console.log("server started",port))
