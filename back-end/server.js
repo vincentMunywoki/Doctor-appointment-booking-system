8c99475fe71052905eae376fa7be4dd4565fb96e
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-18 13:46:30 +0300

    initial commit

--- back-end/server.js
+++ back-end/server.js
@@ -2,12 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 
 // App config
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
+connectCloudinary()
 
 //middlewares
 app.use(express.json())
