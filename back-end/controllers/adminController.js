c5306027cffe8d401f095d96a87d17affef0036a
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-19 13:01:22 +0300

    initial commit

--- back-end/controllers/adminController.js
+++ back-end/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from "validator"
 import bcrypt from 'bcrypt'
 import { v2 as cloudinary } from "cloudinary"
 import doctorModel from "../models/doctorModel.js"
+import jwt from 'jsonwebtoken'
 
 
 // API for ading doctor
@@ -71,6 +72,9 @@ const loginAdmin = async (req, res) => {
         const{email,password} = req.body
 
         if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+
+            const token = jwt.sign(email+password,process.env.JWT_SECRET)
+            res.json({success:true,token})
 
         } else {
             res.json({success:false,message:"Invlid credetials"})
--- back-end/routes/adminRoute.js
+++ back-end/routes/adminRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { addDoctor,loginAdmin } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
+import authAdmin from '../middlewares/authAdmin.js'
 
 const adminRouter = express.Router()
 
-adminRouter.post('/add-doctor',upload.single('image'),addDoctor)
+adminRouter.post('/add-doctor',authAdmin, upload.single('image'),addDoctor)
 adminRouter.post('/login',loginAdmin)
 
 export default adminRouter
