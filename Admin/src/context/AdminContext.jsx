b8bccc7ae9f331678385864eb905e70ebbffce33
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-20 14:27:35 +0300

    initial commit

--- Admin/src/context/AdminContext.jsx
+++ Admin/src/context/AdminContext.jsx
@@ -1,12 +1,17 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
 export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
 
-    const value = {
+    const [aToken,setAToken] = useState('')
 
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const value = {
+        aToken,setAToken,
+        backendUrl,
     }
 
     return(
--- Admin/tailwind.config.js
+++ Admin/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary':"#5F6FFF"
+      }
+    },
   },
   plugins: [],
 }
