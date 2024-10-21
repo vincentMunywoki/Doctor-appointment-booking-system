6613ab9dd90a2d2c805db05333d1361f2d7a3772
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-21 14:25:28 +0300

    initial commit

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -3,14 +3,16 @@ import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
 
 const App = () => {
 
   const { aToken } = useContext(AdminContext)
 
   return aToken ? (
-    <div>
+    <div className='bg-[#f8f9fd]'>
       <ToastContainer />
+      <Navbar />
     </div>
   ) : (
     <>
--- Admin/src/components/Navbar.jsx
+++ Admin/src/components/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { assets } from '../assets/assets'
+import { AdminContext } from '../context/AdminContext'
 
 const Navbar = () => {
+
+    const {aToken} = useContext(AdminContext)
+
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
+        <div className='flex items-center gap-2 text-xs'>
+            <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} alt="" />
+            <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
+        </div>
+        <button className='bg-primary text-white text-sm px-10 py-2 rounded-full'>Logout</button>
+    </div>
   )
 }
 
