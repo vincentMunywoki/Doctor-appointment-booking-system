befc598d836d18bf152f5e6d13147208c7e88ce1
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-22 10:25:37 +0300

    initial commit

--- Admin/src/components/Sidebar.jsx
+++ Admin/src/components/Sidebar.jsx
@@ -7,23 +7,23 @@ const Sidebar = () => {
 
     const { aToken} = useContext(AdminContext)
   return (
-    <div>
+    <div className='min-h-screen bg-white border-r'>
         {
-            aToken && <ul>
-                <NavLink>
+            aToken && <ul className='text-[#515151] mt-5'>
+                <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 boder-primary' : ''}`} to={'/admin-dashboard'}>
                     <img src={assets.home_icon} alt="" />
                     <p>Dashboard</p>
                 </NavLink>
 
-                <NavLink>
+                <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 boder-primary' : ''}`} to={'/all-appointments'}>
                     <img src={assets.appointment_icon} alt="" />
                     <p>Appointment</p>
                 </NavLink>
-                <NavLink>
+                <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 boder-primary' : ''}`} to={'/add-doctor'}>
                     <img src={assets.add_icon} alt="" />
                     <p>Add Doctor</p>
                 </NavLink>
-                <NavLink>
+                <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 boder-primary' : ''}`} to={'/doctor-list'}>
                     <img src={assets.people_icon} alt="" />
                     <p>Doctors List</p>
                 </NavLink>
