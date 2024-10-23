ffb1bb5bd7732b95231e44e30550654323b3dffb
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-23 14:15:21 +0300

    initial commit

--- Admin/src/pages/Admin/AddDoctor.jsx
+++ Admin/src/pages/Admin/AddDoctor.jsx
@@ -3,14 +3,14 @@ import { assets } from '../../assets/assets'
 
 const AddDoctor = () => {
   return (
-    <form action="">
+    <form className='m-5 w-full' >
 
-      <p>Add Doctor</p>
+      <p className='mb-3 text-lg font-medium' >Add Doctor</p>
 
-      <div>
-        <div>
+      <div className='bg-white px-8 py-8 border rounded w-full max-w-4xl max-h-[80vh] overflow-y-scroll' >
+        <div className='flex items-center gap-4 mb-8 text-gray-500'>
           <label htmlFor="doc-img">
-            <img src={assets.upload_area} alt="" />
+            <img className='w-16 bg-gray-100 rounded-full cursor-pointer' src={assets.upload_area} alt="" />
           </label>
           <input type="file" id="doc-img" hidden/>
           <p>Upload Doctor Picture</p>
