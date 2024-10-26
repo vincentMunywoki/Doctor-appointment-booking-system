b560d301cadca0f6eb4ab50cca51c08c5c6e20ed
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-26 10:37:07 +0300

    created doctors list

--- Admin/src/pages/Admin/DoctorsList.jsx
+++ Admin/src/pages/Admin/DoctorsList.jsx
@@ -13,7 +13,24 @@ const DoctorsList = () => {
 
   return (
     <div>
-      
+      <h1>All Doctors</h1>
+      <div>
+        {
+          doctors.map((item,index) =>(
+            <div key={index}>
+              <img src={item.image} alt="" />
+              <div>
+                <p>{item.name}</p>
+                <p>{item.speciality}</p>
+                <div>
+                  <input type="checkbox" checked={item.available} />
+                  <p>Available</p>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
