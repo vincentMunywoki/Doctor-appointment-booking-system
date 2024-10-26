19db195e885c8017b0cf31d750423d711739a2a3
vincent munywoki <vincentmunywoki12@gmail.com>
2024-10-26 10:44:59 +0300

    added css properties for doclist

--- Admin/src/pages/Admin/DoctorsList.jsx
+++ Admin/src/pages/Admin/DoctorsList.jsx
@@ -12,17 +12,17 @@ const DoctorsList = () => {
   },[aToken])
 
   return (
-    <div>
-      <h1>All Doctors</h1>
-      <div>
+    <div className='m-5 max-h-[90vh] overflow-y-scroll'>
+      <h1 className='text-lg font-medium'>All Doctors</h1>
+      <div className='flex flex-wrap gap-4 pt-5 gap-y-6'>
         {
           doctors.map((item,index) =>(
-            <div key={index}>
-              <img src={item.image} alt="" />
-              <div>
-                <p>{item.name}</p>
-                <p>{item.speciality}</p>
-                <div>
+            <div className='border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer group' key={index}>
+              <img className='bg-indigo-50 group-hover:bg-primary transition-all duration-500' src={item.image} alt="" />
+              <div className='p-4'>
+                <p className='text-neutral-800 text-lg font-medium'>{item.name}</p>
+                <p className='text-zinc-600 text-sm'>{item.speciality}</p>
+                <div className='mt-2 flex items-center gap-1 text-sm'>
                   <input type="checkbox" checked={item.available} />
                   <p>Available</p>
                 </div>
