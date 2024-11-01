f87f767ebbbd99f31b6981da0fa2860feb9dcf3b
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-01 11:00:00 +0300

    initial commit

--- front-end/back-end/controllers/adminController.js
+++ front-end/back-end/controllers/adminController.js
@@ -5,6 +5,66 @@ import doctorModel from "../models/doctorModel.js"
 import jwt from 'jsonwebtoken'
 
 
+// API for ading doctor
+const addDoctor = async (req,res) => {
+
+    try {
+
+        const { name, email, password, speciality, degree, experience, about, fees, address} = req.body
+        const imageFile = req.imageFile
+
+        //checking for all data to add doctor
+        if (!name || !email || !password || !speciality || !degree || !experience || !about ||fees || !address) {
+            return res.json({success:false,message:"Missing details"})
+        }
+        
+        // validating email format
+        if (!validator.isEmail(email)) {
+            return res.json({success:false,message:"Please enter a valid email"})
+        }
+
+        // validating strong password
+        if(password.length < 8) {
+            return res.json({success:false,message:"please enter a strong pasword"})
+        }
+
+        // password Encryption Hashing doc password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+
+        // upload image to cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:"image"})
+        const imageUrl = imageUpload.secure_url
+
+        const doctorData = {
+            name,
+            email,
+            image:imageUrl,
+            password:hashedPassword,
+            speciality,
+            degree,
+            experience,
+            about,
+            fees,
+            address:JSON.parse(address),
+            date:Date.now()
+        }
+
+        const newDoctor = new doctorModel(doctorData)
+        //save doctor data in db
+        await newDoctor.save()
+
+
+        res.json({success:true,message:"Doctor Added"})
+
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+
+}
 
 // API for adding a doctor
 const addDoctor = async (req, res) => {
