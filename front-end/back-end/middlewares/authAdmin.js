96a4f178769729585a01055b9204975064ff6da4
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-02 11:35:28 +0300

    initial commit

--- front-end/back-end/middlewares/authAdmin.js
+++ front-end/back-end/middlewares/authAdmin.js
@@ -1,16 +1,13 @@
 import jwt from 'jsonwebtoken'
 
 // admin authentication midleware
-
 const authAdmin = async(req,res,next) => {
     try {
 
         const {atoken} = req.headers
         if (!atoken) {
             return res.json({success:false,message:'Not Authorized, login Again'})
-        }
-              
-
+        }              
         const token_decode = jwt.verify(atoken,process.env.JWT_SECRET)
 
         if(token_decode != process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
---
+++ front-end/back-end/middlewares/authUser.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+
+// user authentication midleware
+const authUser = async(req,res,next) => {
+    try {
+
+        const {token} = req.headers
+        if (!token) {
+            return res.json({success:false,message:'Not Authorized, login Again'})
+        }       
+        const token_decode = jwt.verify(token,process.env.JWT_SECRET)
+
+        
+        next()
+
+    } catch(error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+export default authUser
