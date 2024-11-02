5c976edd37c80a021fcb07147a90091bf9ebcffa
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-02 10:37:50 +0300

    initial commit

--- front-end/front-end/src/main.jsx
+++ front-end/front-end/src/main.jsx
@@ -1,4 +1,4 @@
-/*
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
@@ -14,4 +14,4 @@ createRoot(document.getElementById('root')).render(
   </BrowserRouter>,
 )
 
-*/
+
