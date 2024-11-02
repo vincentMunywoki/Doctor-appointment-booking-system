bd3f2a0c2dbc83591fc8db7ec2e0133b712ec9d8
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-02 10:48:01 +0300

    initial commit

--- front-end/front-end/src/components/Navbar.jsx
+++ front-end/front-end/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {assets} from '../assets/assets'
 import { NavLink, useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
 
 const Navbar = () => {
 
     const navigate = useNavigate();
 
+    const {token,setToken} = useContext(AppContext)
+
     const [showMenu, setShowMenu] = useState(false)
+
+    // const {token,setToken} = useContext(AppContext)
+
 
     const logout = () => {
         setToken('')
@@ -46,7 +52,7 @@ const Navbar = () => {
                         <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4' >
                             <p onClick={()=>navigate('myprofile')} className='hover:text-black cursor-pointer' >My Profile</p>
                             <p onClick={()=>navigate('myappointment')} className='hover:text-black cursor-pointer' >My Appointments</p>
-                            <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer' >Logout</p>
+                            <p onClick={logout} className='hover:text-black cursor-pointer' >Logout</p>
                             
                         </div>
                     </div>
