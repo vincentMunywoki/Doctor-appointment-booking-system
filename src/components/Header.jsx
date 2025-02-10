9fcfa81315027a04dc3fcce2a0399f11de0c868d
vincent munywoki <87576661+vincentMunywoki@users.noreply.github.com>
2025-02-10 22:29:18 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 
+
 const Header = () => {
   return (
     <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20'>  
