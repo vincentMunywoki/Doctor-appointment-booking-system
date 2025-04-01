a975af510ff568b20e6b94d1a367f2214dc4cba0
vincent munywoki <87576661+vincentMunywoki@users.noreply.github.com>
2025-04-01 17:04:18 +0300

    Update Sidebar.jsx

--- front-end/Admin/src/components/Sidebar.jsx
+++ front-end/Admin/src/components/Sidebar.jsx
@@ -10,6 +10,7 @@ const Sidebar = () => {
   return (
     <div className='min-h-screen bg-white border-r'>
         {
+            
             aToken && <ul className='text-[#515151] mt-5'>
                 <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 boder-primary' : ''}`} to={'/admin-dashboard'}>
                     <img src={assets.home_icon} alt="" />
