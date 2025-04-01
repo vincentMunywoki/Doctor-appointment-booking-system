7b62789b0ab07be823d4aa69c0aec7a924a57af9
vincent munywoki <87576661+vincentMunywoki@users.noreply.github.com>
2025-04-01 17:04:30 +0300

    Update Sidebar.jsx

--- front-end/Admin/src/components/Sidebar.jsx
+++ front-end/Admin/src/components/Sidebar.jsx
@@ -17,6 +17,7 @@ const Sidebar = () => {
                     <p>Dashboard</p>
                 </NavLink>
 
+                
                 <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 boder-primary' : ''}`} to={'/all-appointments'}>
                     <img src={assets.appointment_icon} alt="" />
                     <p>Appointment</p>
